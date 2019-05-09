3541ed7828da062c65f29ae39cd4fbe35524ed73
Barry Van Deerlin <bvandeerlin@gmail.com>
2019-05-09 00:11:00 -0700

    remove extra #

--- avremote.py
+++ avremote.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Denon AVR Remote for CLI
-# Copyright (C) 2019  Barry Van Deerlin#
+# Copyright (C) 2019  Barry Van Deerlin
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
