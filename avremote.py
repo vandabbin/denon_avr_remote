75a67fa06eb2a6afa4c036093f1347bb7e1ff0d5
Barry Van Deerlin <bvandeerlin@gmail.com>
2019-05-09 22:43:25 -0700

    fix a silly mistake

--- avremote.py
+++ avremote.py
@@ -243,7 +243,7 @@ class AVRController:
         '''
         resp = self.split(self.recv_status(sock, status_cmd))
         maxVolume = self.recv_status(sock, None, True)
-        if cmd != 'status' or resp == self.ERRORS['2']:
+        if cmd != 'status' or resp != self.ERRORS['2']:
             power_state = self.recv_status(sock, self.SCODES['power'])
             if power_state == self.CODES['on']:
                 if cmd != resp:
@@ -260,7 +260,7 @@ class AVRController:
         Send a mute command and return the response.
         '''
         resp = self.split(self.recv_status(sock, status_cmd))
-        if cmd != 'status' or resp == self.ERRORS['2']:
+        if cmd != 'status' or resp != self.ERRORS['2']:
             power_state = self.recv_status(sock, self.SCODES['power'])
             if power_state == self.CODES['on']:
                 # Toggle mute on or off
@@ -280,7 +280,8 @@ class AVRController:
         Send a source command and return the response.
         '''
         resp = self.split(self.recv_status(sock, status_cmd))
-        if cmd != 'status' or resp == self.ERRORS['2']:
+        if cmd != 'status' or resp != self.ERRORS['2']:
+            print(cmd)
             power_state = self.recv_status(sock, self.SCODES['power'])
             if power_state == self.CODES['on']:
                 if cmd != resp:
